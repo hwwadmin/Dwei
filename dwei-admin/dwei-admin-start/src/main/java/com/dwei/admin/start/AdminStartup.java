1103a133f830fcd0182dff2f61489ebc42f2a896
hww <940806023@qq.com>
2024-08-29 17:57:37 +0800

    init

--- dwei-admin/dwei-admin-start/src/main/java/com/dwei/admin/start/AdminStartup.java
+++ dwei-admin/dwei-admin-start/src/main/java/com/dwei/admin/start/AdminStartup.java
@@ -1,6 +1,7 @@
 package com.dwei.admin.start;
 
 import com.dwei.core.guide.ApplicationBoot;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -17,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAspectJAutoProxy(exposeProxy = true)
 @EnableScheduling
 @EnableAsync
+@MapperScan("com.dwei.**.mapper")
 //@EnableElasticsearchRepositories(basePackages = "com.yomi.boot.*")
 //@EnableDubbo(scanBasePackages = {"com.yomi.boot.*"})
 public class AdminStartup {
